24cc1568012ce203b73efaaaaad5d861190f8fdd
Maca Flo <182832922+MacarenaFlo@users.noreply.github.com>
2025-05-26 18:51:30 -0300

    Buscar Disco
    Buscar disco y corregir errores menores

--- scripts/index.js
+++ scripts/index.js
@@ -70,37 +70,37 @@ function mostrar() {
 }
 
 const pedirTexto = function (msg) {
-    let str;
-    let strValido = false;
+	let str;
+	let strValido = false;
 
-    do {
-        str = prompt(msg);
-        if (str === null) {
-            alert("dato obligatorio, por favor intentelo nuevamente");
-        } else if (str.trim() === "") {
-            alert("no puede dejar el campo vacio, intentelo nuevamente");
-        } else if (!isNaN(str)) {
-            alert("El dato ingresado no puede ser un numero");
-        } else {
-            strValido = true;
-        }
-    } while (!strValido);
-    return str;
+	do {
+		str = prompt(msg);
+		if (str === null) {
+			alert("dato obligatorio, por favor intentelo nuevamente");
+		} else if (str.trim() === "") {
+			alert("no puede dejar el campo vacio, intentelo nuevamente");
+		} else if (!isNaN(str)) {
+			alert("El dato ingresado no puede ser un numero");
+		} else {
+			strValido = true;
+		}
+	} while (!strValido);
+	return str;
 };
 
 const pedirNumero = function (msg) {
-    let num;
-    let numValido = false;
+	let num;
+	let numValido = false;
 
-    do {
-        num = parseInt(prompt(msg));
-        if (isNaN(num)) {
-            alert("el dato ingresado no es valido, intentelo nuevamente");
-        } else {
-            numValido = true;
-        }
-    } while (!numValido);
-    return num;
+	do {
+		num = parseInt(prompt(msg));
+		if (isNaN(num)) {
+			alert("el dato ingresado no es valido, intentelo nuevamente");
+		} else {
+			numValido = true;
+		}
+	} while (!numValido);
+	return num;
 };
 
 function cantidadDiscos() {
@@ -108,10 +108,54 @@ function cantidadDiscos() {
 	div.textContent = "Cantidad de discos: " + discos.length;
 }
 
-//function mostrarDisco(id){
-//
-//}
+function mostrarDisco() {
+	const id = pedirNumero("Ingrese el ID del disco que desea mostrar");
+    let discoEncontrado = false;
+	const div = document.querySelector("#discos");
+	div.innerHTML = ""; // Limpiar el contenido previo
+	discos.forEach((disco) => {
+		if (disco.id === id) {
+            discoEncontrado = true;
+			const discoDiv = document.createElement("div");
+			discoDiv.className = "disco";
+			// Mostrar pistas como lista
+			let pistasHtml = "";
+			if (Array.isArray(disco.pistas)) {
+				pistasHtml = "<ul>";
+				disco.pistas.forEach((pista) => {
+					const minutos = Math.floor(pista.duracion / 60);
+					const segundos = pista.duracion % 60;
+					const duracionFormateada = `${minutos.toString().padStart(2, "0")}:${segundos.toString().padStart(2, "0")}`;
+					const color = pista.duracion > 180 ? " #e62dffbd;" : "white";
+					pistasHtml += `<li style="color: ${color};">${pista.nombre} (${duracionFormateada})</li>`;
+				});
+				pistasHtml += "</ul>";
+			}
+			discoDiv.innerHTML = `
+                <p>Nombre: ${disco.nombre}</p>
+                <p>Artista: ${disco.artista}</p>
+                <p>ID: ${disco.id}</p>
+                <p>Portada:</p>
+                <img src="${disco.portada}" alt="${disco.nombre}" style="max-width: 200px;">
+                <p>Pistas:</p>
+                ${pistasHtml}
+            `;
+			div.appendChild(discoDiv);
+            return;
+		}
+	});
+    if (!discoEncontrado){
+        alert("Disco no encontrado con ID: " + id);
+    }
+}
 
-//const pistaMasLargaTotal = function () {
-//}
-
+const pistaMasLargaTotal = function () {
+    let pistaMasLargaTotal =  new Pista("", -1);
+    discos.forEach((disco) => {
+        let pistaMasLargaDisco = disco.pistaMasLargaDisco();
+        if(pistaMasLargaTotal.duracion < pistaMasLargaDisco.duracion) {
+            pistaMasLargaTotal = pistaMasLargaDisco;
+        }
+    })
+    return pistaMasLargaTotal;
+}
